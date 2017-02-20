7da5a409e8f3dbd3a77d6cca4a445ebc48e952b5
hanguangchao <hanguangchao@gmail.com>
2017-02-20 06:13:28 +0000

    readsl

---
+++ readsl.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+print os.popen('rasdial /DISCONNECT').read()
+time.sleep(3)
+print os.popen('rasdial "设定的宽带名称"  帐号 密码').read()
+print os.popen('rasdial').read()
+time.sleep(1)
